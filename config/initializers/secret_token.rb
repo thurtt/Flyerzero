3bb3eafb02ca494deaefd9389c0de2fb6a10fab3
thurtt <thurtt@gmail.com>
2012-02-29 11:28:39 -0500

    Basic framework for the FlyerZero project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flyerzero::Application.config.secret_token = '8f21efd5592cd1f6b4268259a691e14d94e4976ea328552e7ddee4954c641c5e65f4288a64a7371dd1487e0889c591044f5a291aeeae84204947b0b97f264078'
