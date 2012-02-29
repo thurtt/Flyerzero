f78ec8d9f94da2499b47fbd4bf91abaa9d78f462
thurtt <thurtt@gmail.com>
2012-02-29 11:39:08 -0500

    Adding the capistrano deployment stuff

---
+++ config/deploy.rb
@@ -0,0 +1,51 @@
+# This is used to make sure the custom databse.yml file gets migrated with each deployment
+# There may be a better way to do this, but here is the site I referenced: http://www.simonecarletti.com/blog/2009/06/capistrano-and-database-yml/
+require "./config/capistrano_database"
+
+set :application, "www.flyerzero.com"
+set :repository,  "git://github.com/thurtt/Flyerzero.git"
+set(:user) { Capistrano::CLI.ui.ask("User name: ") }
+set :appuser, "fzu"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm_username, user
+set :runner, user
+set :use_sudo, false
+set :branch, "master"
+set :deploy_via, :checkout
+set :git_shallow_clone, 1
+set :deploy_to, "/home/#{appuser}/#{application}"
+
+
+default_run_options[:pty] = true
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_dsa")]
+role :web, "www.flyerzero.com"                          # Your HTTP server, Apache/etc
+role :app, "www.flyerzero.com"                          # This may be the same as your `Web` server
+role :db,  "www.flyerzero.com", :primary => true # This is where Rails migrations will run
+
+# global rvm integration
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_ruby_string, '1.9.2-p290@rails313'
+set :rvm_type, :system
+
+# This line runs the bundler
+require "bundler/capistrano"
+
+# and this line compiles the asset pipeline
+load 'deploy/assets'
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+end
+
+after "deploy", "deploy:migrate"
