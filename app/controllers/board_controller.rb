84bec5694758359afdd4500385f9979eba969eb4
Stuart Templeton <bvrn007@gmail.com>
2012-03-16 00:10:36 -0400

    foursquare test stuff.

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,4 +1,5 @@
 class BoardController < ApplicationController
+	skip_before_filter :findme, :only=>[:callback ]
 
 	def index
 
@@ -20,6 +21,17 @@ class BoardController < ApplicationController
 		@event = Event.new()
 		@flyers = Event.within(5, :origin => @orgin).where('validated > 0').page(params[:page]).per(10)
 		render :partial=>"flyers"
+	end
+	
+	def venue
+		client = Foursquare2::Client.new(:client_id => 'PD1MFQUHYFZKOWIND0L3AU3HEZ2FHUP1MVJ2BZG0NZXRJ14G', :client_secret => 'UUSATLQWYXAGCOICODDAS1YFUPTHNS4FSFYWONA2SA4VRU0H')
+		#@venues = client.search_venues(:ll => @origin.ll, :query => 'Starbucks')
+		@venues = client.explore_venues(:ll => @origin.ll, :radius => '500')
+		render :text=>@venues.inspect
+	end
+	
+	def callback
+		render :text=>"success"
 	end
 
 end
