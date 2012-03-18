fe4528d1e387391332844db4137db960c901937a
thurtt <thurtt@gmail.com>
2012-03-18 13:31:45 -0400

    Changing the venue search to use the much faster foursquare autocomplete api.

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -25,6 +25,10 @@ class BoardController < ApplicationController
 	end
 
 	def venue
+		if params[:term].length < 3
+		      render :json=>[] and return
+		end
+
 		# foursquare autocomplete endpoint
 		endpoint = 'https://api.foursquare.com/v2/venues/suggestcompletion'
 		response = RestClient.get endpoint, {:params=>{ :ll=>@origin.ll,
@@ -36,27 +40,16 @@ class BoardController < ApplicationController
 		venues = (ActiveSupport::JSON.decode(response))["response"]["minivenues"]
 		venues.map!{ |venue| { :name=>venue["name"],
 				       :cross_street=>venue["location"]["crossStreet"],
-				       :address=>venue[]
+				       :address=>venue["location"]["address"],
+				       :lat=>venue["location"]["lat"],
+				       :lng=>venue["location"]["lng"],
+				       :id=>venue["id"],
+				       :icon=>venue["category"] ? venue["category"]["icon"] : nil
 				     }
 			   }
-
+		render :json=> venues
 	end
-	#def venue
-	#	# ruby 1.9.x on mac needs to know where the certs are
-	#	client = Foursquare2::Client.new(:client_id => 'PD1MFQUHYFZKOWIND0L3AU3HEZ2FHUP1MVJ2BZG0NZXRJ14G',
-	#					 :client_secret => 'UUSATLQWYXAGCOICODDAS1YFUPTHNS4FSFYWONA2SA4VRU0H',
-	#					 :ssl => { :verify => OpenSSL::SSL::VERIFY_PEER, :ca_file => '/opt/local/share/curl/curl-ca-bundle.crt' }
-	#					 )
-	#	@venues = client.search_venues(:ll => @origin.ll, :query => params[:term])
-	#	venue_list = @venues.groups[0].items
-	#	venue_list.map!{ |venue| { :name=>venue.name,
-	#					    :address=>venue.location.address,
-	#					    :cross_street=>venue.location.crossStreet,
-	#					    :lat=>venue.location.lat,
-	#					    :lng=>venue.location.lng,
-	#					    :venue_id=>venue.id } }
-	#	render :json=> venue_list
-	#end
+
 
 	def callback
 		render :text=>"success"
