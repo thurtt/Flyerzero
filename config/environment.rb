2e5ad2befe033b8de9d8a24d94c9f9744b2a8120
thurtt <thurtt@gmail.com>
2012-03-19 23:21:48 -0400

    I broke errrthing. Hopefully this fixes it.

--- config/environment.rb
+++ config/environment.rb
@@ -2,9 +2,9 @@
 require File.expand_path('../application', __FILE__)
 
 # configure action_mailer
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.perform_deliveries = true
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.perform_deliveries = true
 
 # Initialize the rails application
 Flyerzero::Application.initialize!
