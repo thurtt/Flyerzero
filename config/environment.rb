23f0b5d3e33ff7112f7b27baf7b5c909d5f93879
thurtt <thurtt@gmail.com>
2012-03-20 00:19:21 -0400

    This should not be this difficult

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Initialize the rails application
+Flyerzero::Application.initialize!
+
 # configure action_mailer
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.perform_deliveries = true
-
-# Initialize the rails application
-Flyerzero::Application.initialize!
