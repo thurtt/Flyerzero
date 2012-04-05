f970564966f1c0d5dd40ed7786464c6a4a04ff5f
Stuart Templeton <bvrn007@gmail.com>
2012-04-05 14:00:30 -0400

    bug fix

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -11,14 +11,14 @@ class MobileController < ApplicationController
 			@flyer = Event.find(params[:id]) if Event.exists?(params[:id])
 		end
 		
-		@origin = Geokit::LatLng.new(params[:lat], params[:lng])
-		session[:ll] = @origin.ll
+		if params[:lat] != nil && params[:lat] != ''
+			ll = Geokit::LatLng.new(params[:lat], params[:lng])
+		end
 			
-		@event = Event.new()
 		
-		@now = Event.within(5, :origin => session[:ll]).where('validated > 0').where(['expiry > ?', Time.now().beginning_of_day - 1.day]).order('expiry').page(params[:page])
+		@now = Event.within(5, :origin => ll).where('validated > 0').where(['expiry > ?', Time.now().beginning_of_day - 1.day]).order('expiry').page(params[:page])
 		if @now.length < 20
-			@now = Event.within(25, :origin => session[:ll]).where('validated > 0').where(['expiry > ?', Time.now().beginning_of_day - 1.day]).order('expiry').page(params[:page])
+			@now = Event.within(25, :origin => ll).where('validated > 0').where(['expiry > ?', Time.now().beginning_of_day - 1.day]).order('expiry').page(params[:page])
 		end
 		
 		render :json=>@now.to_json(:only => [:id,:lat,:lng,:expiry, :media], :methods => [:map_photo, :map_photo_info])
