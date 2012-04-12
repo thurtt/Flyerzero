f070af07fe10f71ef0a0501897f06f18433c6f6d
Stuart Templeton <bvrn007@gmail.com>
2012-04-12 17:21:21 -0400

    twitter sharing stuff.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -281,10 +281,13 @@ function addAddressToMap(lat, lng, data) {
         	info += '<div style="text-align:left;">';
         	if ( data["profile"] != undefined ){
         		info += '<a href="/profile/view/' + data["profile"] + '" target="_blank">';
-        		info += '<img src="' + data["gravatar"] + '" alt="Submitter Profile"/></a>&nbsp';
+        		info += '<img src="' + data["gravatar"] + '" alt="Submitter Profile" class="box"/></a>&nbsp';
         	}
         	info += '<a href="http://www.facebook.com/sharer.php?&u=http://www.flyerzero.com/?flyer=' + data["flyer_id"] + '&t=Flyer Zero Event" target="_blank">';
-        	info += '<img src="/assets/facebook_share_button.jpeg" alt="Facebook" height=20/></a>';
+        	info += '<img src="/assets/facebook_share.png" alt="Facebook"/></a>&nbsp';
+        	
+        	info += '<a href="https://twitter.com/share?url=http://www.flyerzero.com/?flyer=' + data["flyer_id"] + '" target="_blank">';
+        	info += '<img src="/assets/twitter_share.png" alt="Twitter"/></a>&nbsp';
         	info += '</div>';
         }
         info += '</div>'
