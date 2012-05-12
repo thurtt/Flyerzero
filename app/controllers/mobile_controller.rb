6ed358125e719dfd9c0daaf1bef283c0412d2ae0
thurtt <thurtt@gmail.com>
2012-05-12 02:37:37 -0400

    support for loading a flyer

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -1,26 +1,28 @@
 class MobileController < ApplicationController
-	
+
 	def index
 		render :layout=>"mobile"
 	end
-	
-	def flyers
-		
 
-		if params[:id]
-			@flyer = Event.find(params[:id]) if Event.exists?(params[:id])
-		end
-		
+	def flyers
+
 		if params[:lat] != nil && params[:lat] != ''
 			ll = Geokit::LatLng.new(params[:lat], params[:lng])
 		end
-			
-		
+
+
 		@now = Event.within(5, :origin => ll).where('validated > 0').where(['expiry > ?', Time.now().beginning_of_day - 1.day]).order('expiry').page(params[:page])
 		if @now.length < 20
 			@now = Event.within(25, :origin => ll).where('validated > 0').where(['expiry > ?', Time.now().beginning_of_day - 1.day]).order('expiry').page(params[:page])
 		end
-		
+
 		render :json=>@now.to_json(:only => [:id,:lat,:lng,:expiry, :media, :fbevent, :venue_id], :methods => [:map_photo, :map_photo_info])
+	end
+
+	def flyer
+		if params[:id]
+			@flyer = Event.find(params[:id]) if Event.exists?(params[:id])
+			render :json=>@flyer.to_json(:only => [:id,:lat,:lng,:expiry, :media, :fbevent, :venue_id], :methods => [:map_photo, :map_photo_info])
+		end
 	end
 end
