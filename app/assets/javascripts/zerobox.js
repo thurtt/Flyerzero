d62feac03a6522357e8bfe8d5c7b56e4aa3ae577
Stuart Templeton <bvrn007@gmail.com>
2012-07-13 18:37:14 -0400

    yay!

--- app/assets/javascripts/zerobox.js
+++ app/assets/javascripts/zerobox.js
@@ -1,3 +1,4 @@
+refresh_url = '';
 
 $(document).ready(function() {
     $('.slideshow_container').cycle({
@@ -7,4 +8,46 @@ $(document).ready(function() {
 		    venue_id = currSlideElement.getAttribute('data-venue-id');
 		}
 	});
+    
+    $('#calendar').fullCalendar({
+    		    
+    dayClick: function() {
+        alert($(this).find('.fc-day-number').html());
+    },
+    		    
+    eventRender: function(event, element, view) {
+    	    $('.fc-day' + (event['expiry'].split('-')[2] - 1)).css('background',"url("+event['imgURL']+")");
+    	    $('.fc-day' + (event['expiry'].split('-')[2] - 1)).css('textShadow','-1px 0 1px black, 0 1px 1px black, 1px 0 1px black, 0 -1px 1px black');
+    	    $('.fc-day' + (event['expiry'].split('-')[2] - 1)).css('color',"#fff");
+    	element = null;
+    },
+    events: function(start, end, callback) {
+    	    $('.fc-widget-content').css('color','black');
+    	    $('.fc-widget-content').css('textShadow','');
+    	    $('.fc-widget-content').css('background','');
+        $.ajax({
+            url: refresh_url,
+            dataType: 'json',
+            data: {
+                start: Math.round(start.getTime() / 1000),
+                end: Math.round(end.getTime() / 1000)
+            },
+            success: function(doc) {
+                var events = [];
+                $(doc).each(function() {
+                    events.push({
+                    	className: "zedcal calendar_" + $(this).attr('id'),
+                        expiry: $(this).attr('expiry'),
+                        title: $(this).attr('expiry'),
+                        start: $(this).attr('expiry'),
+                        imgURL: $(this).attr('map_photo') // will be parsed
+                    });
+                });
+                callback(events);
+            }
+        });
+    }
+    });
+    
+    
 });
--- app/controllers/zerobox_controller.rb
+++ app/controllers/zerobox_controller.rb
@@ -12,6 +12,12 @@ class ZeroboxController < ApplicationController
 	  if !params[:promoter_id] or !params[:size] or !params[:all]
 		# not ok
 	  end
+	  
+	  template = "zerobox/slideshow"
+	  
+	  if params[:calendar] == "1"
+	  	  template = "zerobox/calendar"
+	  end
 
 	  promoter_id = params[:promoter_id]
 	  all = params[:all]
@@ -20,10 +26,47 @@ class ZeroboxController < ApplicationController
 	  email = Achievement.find(promoter_id).email
 	  @flyers = Event.where(:email=>email).where('validated > 0').order('expiry') if all
 	  @flyers = Event.where(:email=>email).where('validated > 0').where(['expiry > ?', Time.now().beginning_of_day - 1.day]).order('expiry') if !all
-	  render :template=>"zerobox/slideshow"
+	  render :template=>template
 
   end
-
+  
+  def calendar
+  	  
+  	  if !params[:size]
+  	  	  #not ok
+  	  end
+	  all = params[:all]
+	  @size = params[:size]
+  	  
+  	  if params[:promoter_id]
+  	  	  promoter_id = params[:promoter_id]
+  	  	  refresh_url = "/zerobox/calendar?promoter_id=" + params[:promoter_id] + "&size=" + params[:size]
+	
+		  email = Achievement.find(promoter_id).email
+		  @flyers = Event.where(:email=>email).where('validated > 0').order('expiry') if all
+		  @flyers = Event.where(:email=>email).where('validated > 0').where(['expiry > ?', Time.now().beginning_of_day - 1.day]).order('expiry') if !all
+	  end
+	  
+	  if params[:venue_id]
+		venue_id = params[:venue_id]
+		refresh_url = "/zerobox/calendar?venue_id=" + params[:venue_id] + "&size=" + params[:size]
+		@flyers = Event.where(:venue_id=>venue_id).where('validated > 0').where(['expiry > ?', Time.now().beginning_of_day - 1.day]).order('expiry')
+	  end
+	  
+	  if params[:ll]
+	  	  ll = params[:ll]
+  	  	  refresh_url = "/zerobox/calendar?ll=" + params[:ll] + "&size=" + params[:size] + "&radius=" + params[:radius]
+	  	  radius = params[:radius]
+	  	  @flyers = Event.within(radius, :origin => ll).where('validated > 0').where(['expiry > ?', Time.now().beginning_of_day - 1.day]).order('expiry')
+	  end
+	  
+	  respond_to do |format|
+	  	  format.html { render :template=>"zerobox/calendar", :locals=>{:refresh_url=>refresh_url}}
+	  	  format.json { render :json=>@flyers.to_json(:only => [:id,:lat,:lng,:expiry, :media, :fbevent, :venue_id], :methods => [:map_photo, :map_photo_info]) }
+	  end
+	  
+	  
+  end
 
   def venue
 	  # Venue: Show all upcoming shows at a venue
