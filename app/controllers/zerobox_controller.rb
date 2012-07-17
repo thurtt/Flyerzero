9896a6c5cda63bad3d1d5a437712e550d4c2b262
thurtt <thurtt@gmail.com>
2012-07-17 15:38:43 -0400

    refactored zerobox and calendar code

--- app/controllers/zerobox_controller.rb
+++ app/controllers/zerobox_controller.rb
@@ -2,35 +2,21 @@ class ZeroboxController < ApplicationController
   def index
 
   end
-  
+
   def calendar
   	  if !params[:size]
   	  	  #not ok
   	  end
-	  all = params[:all]
 	  @size = params[:size]
 
-  	  if params[:promoter_hash]
-  	  	  promoter_hash = params[:promoter_hash]
+	  # promoter
+	  @flyers = Event.by_promoter(params[:promoter_hash]).is_valid.in_range(params[:start], params["end"]) if params[:promoter_hash]
 
-		  result = Achievement.where(:gravatar_hash=>promoter_hash)
-		  if result
-			email = result[0].email
-			@flyers = Event.where(:email=>email).where('validated > 0').order('expiry') if all
-			@flyers = Event.where(:email=>email).where('validated > 0').where(['expiry >= ? && expiry <= ?', params[:start], params["end"]]).order('expiry') if !all
-		  end
-	  end
+	  # venue
+	  @flyers = Event.by_venue(params[:venue_id]).is_valid.in_range(params[:start], params["end"]) if params[:venue_id]
 
-	  if params[:venue_id]
-		venue_id = params[:venue_id]
-		@flyers = Event.where(:venue_id=>venue_id).where('validated > 0').where(['expiry >= ? && expiry <= ?', params[:start], params["end"]]).order('expiry')
-	  end
-
-	  if params[:ll]
-	  	  ll = params[:ll]
-	  	  radius = params[:radius]
-	  	  @flyers = Event.within(radius, :origin => ll).where('validated > 0').where(['expiry >= ? && expiry <= ?', params[:start], params["end"]]).order('expiry')
-	  end
+	  # point-radius
+	  @flyers = Event.by_ll_and_radius(params[:ll], params[:radius]).is_valid.in_range(params[:start], params["end"]) if params[:ll]
 
 	  respond_to do |format|
 	  	  format.html { render :template=>"zerobox/calendar"}
@@ -46,17 +32,18 @@ class ZeroboxController < ApplicationController
 	  @size = params[:size]
 
 	  # A promoter slideshow
-	  @flyers = Event.valid_by_promoter(params[:promoter_hash]) if params[:promoter_hash] and all
-	  @flyers = Event.current_and_valid_by_promoter(params[:promoter_hash]) if params[:promoter_hash] and !all
+	  @flyers = Event.by_promoter(params[:promoter_hash]).is_valid if params[:promoter_hash] and all
+	  @flyers = Event.by_promoter(params[:promoter_hash]).is_valid.is_current if params[:promoter_hash] and !all
 
 	  # A point-radius slideshow
-	  @flyers = Event.current_and_valid_by_ll_and_radius(params[:ll], params[:radius]) if params[:ll]
+	  @flyers = Event.by_ll_and_radius(params[:ll], params[:radius]).is_valid.is_current if params[:ll]
 
 	  # A venue oriented slideshow
-	  @flyers = Event.current_and_valid_by_venue(params[:venue_id]) if params[:venue_id]
+	  @flyers = Event.by_venue(params[:venue_id]).is_valid.is_current if params[:venue_id]
 
 	  respond_to do |format|
-	  	  format.html { render :template=>"zerobox/slideshow", :locals=>{:refresh_url=>refresh_url}}
+	  	  format.html { render :template=>"zerobox/slideshow"}
 	  	  format.json { render :json=>@flyers.to_json(:only => [:id,:lat,:lng,:expiry, :media, :fbevent, :venue_id], :methods => [:map_photo, :map_photo_info]) }
 	  end
   end
+end
--- app/models/event.rb
+++ app/models/event.rb
@@ -12,22 +12,14 @@ class Event < ActiveRecord::Base
 
 	attr_accessor :distance_from_object
 
-	scope :current_and_valid_by_ll_and_radius, lambda do |ll, radius|
-	  within(radius, :origin => ll).where('validated > 0').where(['expiry > ?', Time.now().beginning_of_day - 1.day]).order('expiry')
-	end
-
-	scope :valid_by_promoter, lambda do |promoter_hash|
-	    result = Achievement.find_by_gravatar_hash(promoter_hash)
-	    where(:email=>result.email).where('validated > 0').order('expiry')
-	end
-
-	scope :current_and_valid_by_promoter, lambda do |promoter_hash|
-	    valid_by_promoter(promoter_hash).where(['expiry > ?', Time.now().beginning_of_day - 1.day]).order('expiry')
-	end
-
-	scope :current_and_valid_by_venue, lambda do |venue_id|
-	    where(:venue_id=>venue_id).where('validated > 0').where(['expiry > ?', Time.now().beginning_of_day - 1.day])
-	end
+	scope :is_current, lambda {where(['expiry > ?', Time.now().beginning_of_day - 1.day]).order('expiry')}
+	scope :is_valid, lambda {where('validated > 0').order('expiry')}
+	scope :in_range, lambda {|start, stop| where(['expiry >= ? && expiry <= ?', start, stop]).order('expiry')}
+	scope :by_ll_and_radius, lambda {|ll, radius| within(radius, :origin => ll).order('expiry')}
+	scope :by_promoter, lambda {|promoter_hash|
+		    result = Achievement.find_by_gravatar_hash(promoter_hash)
+		    where(:email=>result.email).order('expiry') }
+	scope :by_venue, lambda{|venue_id| where(:venue_id=>venue_id).order('expiry')}
 
 	def get_distance_from(point = nil)
 		point ||= distance_from_object
