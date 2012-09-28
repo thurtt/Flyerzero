d1448e6a42376e7e050684fba12ec737ba8229f0
Stuart Templeton <bvrn007@gmail.com>
2012-09-28 12:55:09 -0400

    clean-up

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -15,16 +15,10 @@ class ProfileController < ApplicationController
 		if !params[:email]
 			render(:text=>"Invalid Request", :status => 403) and return
 		end
-		
-		
+			
 		@flyers = Event.where(:email=>params[:email], :validated=>1).order('expiry desc')
 		
 		render :json=>@flyers.to_json(:only => [:id,:lat,:lng,:expiry, :media, :fbevent, :venue_id], :methods => [:map_photo, :map_photo_info, :gravatar, :promoter]) 
-		
-		#respond_to do |format|
-		#	format.json{render :json=>@flyers.to_json(:only => [:id,:lat,:lng,:expiry, :media, :fbevent, :venue_id], :methods => [:map_photo, :map_photo_info, :gravatar, :promoter]) }
-		#	format.html{render}
-		#end
 	end
 	
 end
